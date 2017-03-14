8a9a1a4489212c52f1d7d9a6fd5ca07784866498
doverh <doverheld@gmail.com>
2017-03-14 12:15:32 -0400

    Adding pages to Sinatra app

--- app.rb
+++ app.rb
@@ -4,26 +4,40 @@ require_relative 'human_player.rb'
 require_relative 'game.rb'
 require_relative 'random_player.rb'
 require_relative 'sequential_player.rb'
-require_relative 'Unbeatable_player_player.rb'
+require_relative 'unbeatable_player.rb'
 
 
 enable 'sessions'
 
 get '/' do
 	
-	erb:ttt_main
+	erb:ttt_board
 
 end
 
  post '/getLevel' do
  	 session[:player1] = "Human_player"
  	 session[:player2] = params["level"]
- 	 session[:results] = play(player1,player2)
-	 # res =  calc_change(coin.to_f)
+ 	 # res =  calc_change(coin.to_f)
 	 # session[:results] = res.assoc("quarters")		 
 	 # teste = res.assoc("quarters")
-	 erb:ttt_main, :locals => {:results=>session[:results]}
-
+	 erb:ttt_board, :locals => {:player1=>session[:player1], :player2=>session[:player2]}
 end
- 	
+
+ get '/getLevel' do
+     player1 = session[:player1]
+     player2 = session[:player2]
+ 	 board = Board.new
+	  
+ 	 while board.anyMoveLeft?()
+     	position1 = player1.getMove(board)
+     	#send position1 to the interface
+     	position2 = player2.getMove(board)
+    	 #send position1 to the interface
+     end
+     erb:ttt_board, :locals => {:board=>board}
+	
+end
+
+	
 	
